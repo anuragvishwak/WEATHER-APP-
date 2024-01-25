782d0757780f4cdda9a0d089ec8fd6896d076bb5
anuragvishwak <87541383+anuragvishwak@users.noreply.github.com>
2024-01-25 11:12:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import WeatherPage from './weather-Page-1';
 
 function App() {
   return (
     <div className="App">
-      <WeatherPage/>
+      hello
     </div>
   );
 }
--- src/weather-Page-1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function WeatherPage() {
-  return (
-    <div>STARTING WITH A NEW PROJECT</div>
-  )
-}
-
-export default WeatherPage
